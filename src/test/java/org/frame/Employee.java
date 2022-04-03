67631742a81241ec4cf94fc9a0db8a6822628945
arsath3035 <usern8280@gmail.com>
2022-04-03 11:13:49 +0530

    FrameWorkClass1 is added

---
+++ src/test/java/org/frame/Employee.java
@@ -0,0 +1,33 @@
+package org.frame;
+
+import org.testng.annotations.Test;
+
+public class Employee {
+	@Test
+	public void Tc21() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test(invocationCount = 5)
+	public void Tc22() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test (priority= 5)
+	public void Tc23() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test (enabled = false)
+	public void Tc24() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test
+	public void Tc25() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
---
+++ src/test/java/org/frame/FrameWorkClass1.java
@@ -0,0 +1,34 @@
+package org.frame;
+
+import org.testng.annotations.Test;
+
+public class FrameWorkClass1 {
+	
+	@Test
+	public void Tc1() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test (priority=8)
+	public void Tc2() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test(priority=10)
+	public void Tc3() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test (enabled=false)
+	public void Tc4() {
+		// TODO Auto-generated method stub
+
+	}
+	@Test(enabled=true)
+	public void Tc5() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
